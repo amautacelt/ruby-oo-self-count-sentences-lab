08c5d50db49d230a0b0396411f7bed274c115201
Carlos E. La Torre <amautacelt@gmail.com>
2021-02-24 00:39:10 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,27 @@
-require 'pry'
+# require 'pry'
 
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    # self.split(/[.?!]/).delete_if{|n| n==''}.count
+    count = 0
+    self.split.each do |word|
+      if word.sentence? || word.question? || word.exclamation?
+        count += 1
+      end
+    end
+    count
   end
 end
